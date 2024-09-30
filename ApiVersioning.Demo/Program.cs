fedd87061d7e95cb32bb08231565077257cff557
“Ervis <ervistrupja@outlook.com>
2024-09-30 10:13:15 +0200

    setting up the project to support api versioning

--- ApiVersioning.Demo/Program.cs
+++ ApiVersioning.Demo/Program.cs
@@ -12,9 +12,9 @@ builder.Services.AddApiVersioning(options =>
     options.AssumeDefaultVersionWhenUnspecified = true;
     options.DefaultApiVersion = new ApiVersion(1, 0);
     options.ReportApiVersions = true;
-    //options.ApiVersionReader = new UrlSegmentApiVersionReader();
-    options.ApiVersionReader = ApiVersionReader.Combine(
-       new UrlSegmentApiVersionReader());
+}).AddApiExplorer(options => {
+    options.GroupNameFormat = "'v'VVV"; 
+    options.SubstituteApiVersionInUrl = true; 
 });
 
 
